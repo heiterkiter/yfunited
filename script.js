1a951752cc5c7eeae09b7876c94bbae8954e52bf
heiterkiter <53449596+heiterkiter@users.noreply.github.com>
2021-05-12 16:42:16 +0000

    Deploying to gh-pages from master @ 02ef0c6085eca3072cb5fad4f424768c9caa0ec4 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://www.yfu.de")})();
+(()=>{"use strict";console.log("Script started successfully"),WA.openCoWebSite("https://workadventu.re")})();
 //# sourceMappingURL=script.js.map
