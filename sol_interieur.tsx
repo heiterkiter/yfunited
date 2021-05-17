2bcec88cb43eb6e4b954017b1aad4915646893ba
heiterkiter <53449596+heiterkiter@users.noreply.github.com>
2021-05-17 20:34:24 +0200

    fgdfg

---
+++ sol_interieur.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="sol_interieur" tilewidth="32" tileheight="32" tilecount="108" columns="12">
+ <image source="../../../Desktop/workadventure-develop/maps/Village/sol_interieur.png" width="384" height="288"/>
+</tileset>
